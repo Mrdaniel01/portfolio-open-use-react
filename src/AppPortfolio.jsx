3334830cc6d59687b9630a4b54448babf7a44e35
Juan Daniel Rios Ramirez <mrdaniel19996@gmail.com>
2020-11-12 18:21:12 -0500

    [ADD]Initil Commit, base init

---
+++ src/AppPortfolio.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function AppPortfolio() {
+  return (
+    <div className="App">
+      <h1>My Port</h1>
+    </div>
+  );
+}
+
+export default AppPortfolio;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/styles.scss';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import AppPortfolio from './AppPortfolio';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppPortfolio />
   </React.StrictMode>,
   document.getElementById('root')
 );
